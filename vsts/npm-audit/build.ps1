7f30a9a840ab13b20ddbe39f66961bcd281cd40c
jonsth131 <jonsth131@studentmail.oru.se>
2019-06-17 21:40:52 +0200

    Update npm-audit package

--- vsts/npm-audit/build.ps1
+++ vsts/npm-audit/build.ps1
@@ -19,6 +19,7 @@ Write-Output "Copying files to compile folder"
 Set-Location ..
 Copy-Item .\$sourceDir\*.js $taskOutputDir
 Copy-Item .\$sourceDir\*.json $taskOutputDir
+Copy-Item .\$sourceDir\*.png $taskOutputDir
 Copy-Item .\package.json $taskOutputDir
 
 Write-Output "Restoring dependencies"
@@ -32,7 +33,5 @@ Remove-Item .\package-lock.json
 Write-Output "Creating vsix package"
 Set-Location ..
 & tfx extension create --manifest-globs vss-extension.json
-
-Write-Output "Created $extensionType extension $extension"
 
 Set-Location $PSScriptRoot
